35c1e786d05c7db09c3b9fd6fd8f03ea3d9de6d9
krneki777 <theweirdguy247@gmail.com>
2018-01-09 15:37:15 +0100

    en programček

---
+++ temperature.py
@@ -0,0 +1,13 @@
+from math import ceil, floor
+
+x = float(input("Vstavi temperaturo:"))
+if -0.5 <= x < 0:
+	print ("-0")
+
+else:
+	decimalni_del = x - floor(x)
+	if decimalni_del >= 0.5:
+		print (ceil(x))
+
+	else:
+		print(round(x))
